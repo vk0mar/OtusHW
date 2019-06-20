728a037dbbb641d34fcaf1da75fb41035e2bc406
Vladimir <formeonly@mail.ru>
2019-06-20 11:36:32 +0400

    fix

--- otushomework2/src/test/java/TestOtus.java
+++ otushomework2/src/test/java/TestOtus.java
@@ -4,13 +4,13 @@ import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
+
 
 public class TestOtus {
     private WebDriver driver;
